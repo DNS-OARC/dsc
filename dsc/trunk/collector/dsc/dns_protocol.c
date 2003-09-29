b32a55d79079473c91a34076d9d0330aef141317
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2003-09-29 22:53:57 +0000

    bug: must use ntohs() on the area part of the DNS message header containing QR, RCODE, etc.  Also decided to get rid of the 'rfc1035_header' structure, which wasn't really being used here.

--- dsc/trunk/collector/dsc/dns_protocol.c
+++ dsc/trunk/collector/dsc/dns_protocol.c
@@ -6,30 +6,12 @@
 #include <assert.h>
 #include <ctype.h>
 
-
 #include "string_counter.h"
 #include "dns_message.h"
 
-
-
-typedef struct _rfc1035_header rfc1035_header;
-struct _rfc1035_header {
-    unsigned short id;
-    unsigned int qr:1;
-    unsigned int opcode:4;
-    unsigned int aa:1;
-    unsigned int tc:1;
-    unsigned int rd:1;
-    unsigned int ra:1;
-    unsigned int rcode:4;
-    unsigned short qdcount;
-    unsigned short ancount;
-    unsigned short nscount;
-    unsigned short arcount;
-};
-
-
+#define DNS_MSG_HDR_SZ 12
 #define RFC1035_MAXLABELSZ 63
+
 static int
 rfc1035NameUnpack(const char *buf, size_t sz, off_t * off, char *name, size_t ns
 )
@@ -87,51 +69,47 @@ rfc1035NameUnpack(const char *buf, size_t sz, off_t * off, char *name, size_t ns
 dns_message *
 handle_dns(const char *buf, int len)
 {
-    rfc1035_header qh;
     unsigned short us;
     off_t offset;
     char *t;
     int x;
     dns_message *m = calloc(1, sizeof(*m));
 
-    if (len < sizeof(qh)) {
+    if (len < DNS_MSG_HDR_SZ) {
 	free(m);
 	return NULL;
     }
-#if 0
-    memcpy(&us, buf + 00, 2);
-    qh.id = ntohs(us);
-#endif
-
     memcpy(&us, buf + 2, 2);
-    qh.qr = (us >> 15) & 0x01;
+    us = ntohs(us);
+    m->qr = (us >> 15) & 0x01;
+
 #if 0
-    qh.opcode = (us >> 11) & 0x0F;
-    qh.aa = (us >> 10) & 0x01;
-    qh.tc = (us >> 9) & 0x01;
-    qh.rd = (us >> 8) & 0x01;
-    qh.ra = (us >> 7) & 0x01;
+    opcode = (us >> 11) & 0x0F;
+    aa = (us >> 10) & 0x01;
+    tc = (us >> 9) & 0x01;
+    rd = (us >> 8) & 0x01;
+    ra = (us >> 7) & 0x01;
 #endif
-    qh.rcode = us & 0x0F;
-
-    memcpy(&us, buf + 4, 2);
-    qh.qdcount = ntohs(us);
+    m->rcode = us & 0x0F;
 
 #if 0
+    memcpy(&us, buf + 4, 2);
+    qdcount = ntohs(us);
+
     memcpy(&us, buf + 6, 2);
-    qh.ancount = ntohs(us);
+    ancount = ntohs(us);
 
     memcpy(&us, buf + 8, 2);
-    qh.nscount = ntohs(us);
+    nscount = ntohs(us);
 
     memcpy(&us, buf + 10, 2);
-    qh.arcount = ntohs(us);
+    arcount = ntohs(us);
 #endif
 
     /*
      * XXX assume there is exactly one query
      */
-    offset = sizeof(qh);
+    offset = DNS_MSG_HDR_SZ;
     x = rfc1035NameUnpack(buf, len, &offset, m->qname, MAX_QNAME_SZ);
     if (0 != x) {
 	free(m);
@@ -151,9 +129,6 @@ handle_dns(const char *buf, int len)
     m->qtype = ntohs(us);
     memcpy(&us, buf + offset + 2, 2);
     m->qclass = ntohs(us);
-
-    m->rcode = qh.rcode;
-    m->qr = qh.qr;
 
     return m;
 }
