8694780b83ce2e0f361c4535910c7a457b5c37a8
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2003-11-13 16:39:04 +0000

    adding qnamelen indexer

---
+++ dsc/trunk/collector/dsc/qnamelen_index.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "dns_message.h"
+#include "md_array.h"
+static int largest = 0;
+
+int
+qnamelen_indexer(dns_message * m)
+{
+    int i = strlen(m->qname);;
+    if (i >= MAX_QNAME_SZ)
+	i = MAX_QNAME_SZ - 1;
+    if (i > largest)
+	largest = i;
+    return i;
+}
+
+static int next_iter;
+
+int
+qnamelen_iterator(char **label)
+{
+    static char label_buf[10];
+    if (NULL == label) {
+	next_iter = 0;
+	return largest;
+    }
+    if (next_iter > largest)
+	return -1;
+    snprintf(label_buf, 10, "%d", next_iter);
+    *label = label_buf;
+    return next_iter++;
+}
