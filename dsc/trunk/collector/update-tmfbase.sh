fd88ddb9c0fca36864957d4c5279686599d2d6aa
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2003-12-03 18:22:29 +0000

    Make Makefile compile everything, including TmfBase stuff

--- dsc/trunk/collector/update-tmfbase.sh
+++ dsc/trunk/collector/update-tmfbase.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 #cvs status TmfBase| grep ^File | grep -v Up-to-date
+mkdir tmp
+cd tmp
 cvs -d :ext:sparky:/usr/local/CVS export -N -kv -r HEAD TmfBase/Hapy
 cvs -d :ext:sparky:/usr/local/CVS export -N -kv -r HEAD TmfBase/xstd
+rsync -av tmp/TmfBase .
