eb454d5eef770689c472cb6d7fcd2d1a96089b6c
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2004-02-26 17:21:37 +0000

    xstd no longer needed by Hapy

--- dsc/trunk/collector/TmfBase/xstd/UniqueNum.h
+++
@@ -1,9 +0,0 @@
- /* Made at The Measurement Factory     ::     measurement-factory.com */
-
-#ifndef TMF_BASE__XSTD_UNIQUE_NUM__H
-#define TMF_BASE__XSTD_UNIQUE_NUM__H
-
-// _should_ be unique across all programs and all invocations
-extern long int UniqueNum();
-
-#endif
