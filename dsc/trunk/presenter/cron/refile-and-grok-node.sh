a8ef733728c20a5579afc37e85597e44feee61a2
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2004-03-26 00:34:16 +0000

    split 'refile-and-grok' into two scripts for easier parallelization

---
+++ dsc/trunk/presenter/cron/refile-and-grok-node.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+
+: ${SERVER:?}
+: ${NODE:?}
+: ${EXECDIR:?}
+: ${TYPES:?}
+
+PROG=`basename $0`
+PIDF="/var/tmp/$PROG-$SERVER-$NODE.pid"
+if test -f $PIDF ; then
+	PID=`cat $PIDF`
+	if test -n "$PID" ; then
+		if kill -0 $PID ; then
+			exit 0
+		fi
+	fi
+fi
+
+trap "rm -f $PIDF" EXIT
+echo $$ > $PIDF
+
+exec >$PROG.stdout
+#exec 2>&1
+#set -x
+date
+
+
+for type in $TYPES ; do
+	xmls=`ls -r | grep "\.$type\.xml$" | head -5` || true
+	if test -n "$xmls" ; then
+		for h in $xmls ; do
+			secs=`echo $h | awk -F. '{print $1}'`
+			yymmdd=`date -u -r $secs +%Y%m%d`
+			test -d $yymmdd || mkdir $yymmdd
+			test -d $yymmdd/$type || mkdir $yymmdd/$type
+			$EXECDIR/$type-extractor.pl $h
+			mv $h $yymmdd/$type
+		done
+	fi
+done
+date
--- dsc/trunk/presenter/cron/refile-and-grok.sh
+++ dsc/trunk/presenter/cron/refile-and-grok.sh
@@ -1,59 +1,28 @@
 #!/bin/sh
-
 set -e
-EXECDIR=/usr/local/dsc/libexec
 
 cd /data/dns-oarc
 
 PROG=`basename $0`
-PIDF="/var/tmp/$PROG.pid"
-if test -f $PIDF ; then
-	PID=`cat $PIDF`
-	if test -n "$PID" ; then
-		if kill -0 $PID ; then
-			exit 0
-		fi
-	fi
-fi
-
-trap "rm -f $PIDF" EXIT
-echo $$ > $PIDF
-
-exec >`basename $0`.stdout
+exec >$PROG.stdout
 #exec 2>&1
 #set -x
 date
 
-uptime >> /httpd/htdocs/loadav/loadav.log
+EXECDIR=/usr/local/dsc/libexec
+TYPES=`ls $EXECDIR/*-extractor.pl | awk -F/ '{print $NF}' | awk -F- '{print $1}'`
+export EXECDIR SERVER NODE TYPES
 
-for server in * ; do
-	test -d $server || continue;
-	cd $server
-	for node in * ; do
-		
-		cd $node
-
-		echo "$server/$node:"
-
-		xmls=`ls -r | grep xml$ | head -50` || true
-		if test -n "$xmls" ; then
-		    for h in $xmls ; do
-			secs=`echo $h | awk -F. '{print $1}'`
-			type=`echo $h | awk -F. '{print $2}'`
-			yymmdd=`date -u -r $secs +%Y%m%d`
-			test -d $yymmdd || mkdir $yymmdd
-			test -d $yymmdd/$type || mkdir $yymmdd/$type
-			if test -f $EXECDIR/$type-extractor.pl ; then
-				$EXECDIR/$type-extractor.pl $h
-				mv $h $yymmdd/$type
-			fi
-
-		    done
-		fi
-
-		cd ..	# node
-
+for SERVER in * ; do
+	test -d $SERVER || continue;
+	cd $SERVER
+	for NODE in * ; do
+		cd $NODE
+		echo "$SERVER/$NODE:"
+		sh ../../cron-1min-node.sh &
+		cd ..	# NODE
 	done
-	cd ..	# server
+	cd ..	# SERVER
 done
+wait
 date
