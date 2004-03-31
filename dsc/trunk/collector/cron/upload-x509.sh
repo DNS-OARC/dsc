96142123f3e665275c25783912a0a201bee4ef5f
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2004-03-31 21:26:44 +0000

    move sleep
    use ls -r instead of ls -t

--- dsc/trunk/collector/cron/upload-x509.sh
+++ dsc/trunk/collector/cron/upload-x509.sh
@@ -21,16 +21,16 @@ trap "rm -f $PIDF" EXIT
 CURL="curl --silent"
 SRVAUTH="--cacert /usr/local/dsc/etc/cacert.pem"
 URI="https://dns-oarc.measurement-factory.com"
+sleep 3
 
 for node in hq lgh sc ; do
 	CLTAUTH="--cert /usr/local/dsc/etc/$node-cert.pem"
 	cd /usr/local/dsc/run/$node
-	sleep 5
 
 	exec > $PROG.out
 	exec 2>&1
 
-	k=`ls -t | grep xml$ | head -500` || true
+	k=`ls -r | grep xml$ | head -500` || true
 	test -n "$k" || continue
 	TF=`mktemp /tmp/put.XXXXXXXXXXXXX`
 	tar czf $TF $k
