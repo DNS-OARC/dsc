ef4a716c60d48e701facf1c27dd97be967911758
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2004-04-01 17:35:14 +0000

    fixed another DNS message parsing bug.  Found a query with compression ptr == 0.

--- dsc/trunk/collector/dsc/dns_protocol.c
+++ dsc/trunk/collector/dsc/dns_protocol.c
@@ -20,8 +20,7 @@ rfc1035NameUnpack(const char *buf, size_t sz, off_t * off, char *name, int ns)
     unsigned char c;
     size_t len;
     if (ns <= 0) {
-	/* probably compression loop */
-	return 4;
+	return 4; /* probably compression loop */
     }
     do {
 	if ((*off) >= sz)
@@ -36,18 +35,20 @@ rfc1035NameUnpack(const char *buf, size_t sz, off_t * off, char *name, int ns)
 	    (*off) += sizeof(s);
 	    /* Sanity check */
 	    if ((*off) >= sz)
-		return 1;
+		return 1;	/* message too short */
 	    ptr = s & 0x3FFF;
 	    /* Make sure the pointer is inside this message */
 	    if (ptr >= sz)
-		return 2;
+		return 2;	/* bad compression ptr */
+	    if (ptr < DNS_MSG_HDR_SZ)
+		return 2;	/* bad compression ptr */
 	    return rfc1035NameUnpack(buf, sz, &ptr, name + no, ns - no);
 	} else if (c > RFC1035_MAXLABELSZ) {
 	    /*
 	     * "(The 10 and 01 combinations are reserved for future use.)"
 	     */
 	    break;
-	    return 3;
+	    return 3;		/* reserved label/compression flags */
 	} else {
 	    (*off)++;
 	    len = (size_t) c;
@@ -55,8 +56,8 @@ rfc1035NameUnpack(const char *buf, size_t sz, off_t * off, char *name, int ns)
 		break;
 	    if (len > (ns - 1))
 		len = ns - 1;
-	    if ((*off) + len > sz)	/* message is too short */
-		return 4;
+	    if ((*off) + len > sz)
+		return 4;		/* message is too short */
 	    memcpy(name + no, buf + (*off), len);
 	    (*off) += len;
 	    no += len;
