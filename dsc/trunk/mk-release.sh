7c0bd7aa8ac7ef4aaf0ed5c02c9e88d16480560d
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2004-08-24 20:59:15 +0000

    a shell script for generating release tarballs

---
+++ dsc/trunk/mk-release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+RM="rm -rf"
+TD=`mktemp -d /tmp/XXXXXXXXXX`
+trap '${RM} $TD' EXIT
+
+if test -f CVS/Root ; then
+	CVSROOT=`cat CVS/Root`
+	export CVSROOT
+fi
+
+cd $TD
+TS=`date +%Y%m%d%H%M`
+cvs export -r HEAD -d dsc-$TS dsc
+${RM} update-tmfbase.sh
+${RM} mk-release.sh
+${RM} junk
+
+(cd dsc-$TS/doc; make dsc-manual.ps dsc-manual.pdf clean-release)
+tar czvf ../dsc-$TS.tar.gz dsc-$TS
