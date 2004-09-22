cd9cd8c24511510c3dd945debb63029240013e31
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2004-09-22 02:20:47 +0000

    changes inspired by bug with using 'continue' outside of for loop. This new version is based on whats running on f-root collectors

--- dsc/trunk/collector/cron/upload-x509.sh
+++ dsc/trunk/collector/cron/upload-x509.sh
@@ -6,6 +6,10 @@ PATH=/usr/bin:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
 PROG=`basename $0`
 
+NODE=$1; shift
+DEST=$1; shift
+URI=$1; shift
+
 PIDF="/tmp/$PROG.pid"
 if test -f $PIDF; then
 	PID=`cat $PIDF`
@@ -19,25 +23,22 @@ echo $$ >$PIDF
 trap "rm -f $PIDF" EXIT
 
 CURL="curl --silent"
+CLTAUTH="--cert $HOME/dsc/etc/$DEST-certs/$NODE-cert.pem"
 SRVAUTH="--cacert /usr/local/dsc/etc/cacert.pem"
-URI="https://dns-oarc.measurement-factory.com"
-sleep 3
+perl -e 'sleep((rand 10) + 5)'
 
-for node in hq lgh sc ; do
-	CLTAUTH="--cert /usr/local/dsc/etc/$node-cert.pem"
-	cd /usr/local/dsc/run/$node
+cd /usr/local/dsc/run/$NODE
 
-	exec > $PROG.out
-	exec 2>&1
+exec > $PROG.out
+exec 2>&1
 
-	k=`ls -r | grep xml$ | head -500` || true
-	test -n "$k" || continue
-	md5 -r $k > MD5s
-	TF=`mktemp /tmp/put.XXXXXXXXXXXXX`
-	tar czf $TF MD5s $k
-	mv $TF $TF.tar
-	TF="$TF.tar"
-	UPLOAD="--upload $TF"
-	$CURL $SRVAUTH $CLTAUTH $UPLOAD $URI | awk '$1 == "Stored" {print $2}' | xargs rm -v
-	rm -f $TF
-done
+k=`ls -r | grep xml$ | head -500` || true
+test -n "$k" || exit 0
+md5 -r $k > MD5s
+TF=`mktemp /tmp/put.XXXXXXXXXXXXX`
+tar czf $TF MD5s $k
+mv $TF $TF.tar
+TF="$TF.tar"
+UPLOAD="--upload $TF"
+$CURL $SRVAUTH $CLTAUTH $UPLOAD $URI | awk '$1 == "Stored" {print $2}' | xargs rm -v
+rm -f $TF
