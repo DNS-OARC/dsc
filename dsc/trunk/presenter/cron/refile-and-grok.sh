f3ae8802fa09b181039776f2fc366ac1174a4e0b
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2005-02-21 23:09:45 +0000

    Test that directories are actually directories, and not symlinks

--- dsc/trunk/presenter/cron/refile-and-grok.sh
+++ dsc/trunk/presenter/cron/refile-and-grok.sh
@@ -14,9 +14,12 @@ TYPES=`ls $EXECDIR/*-extractor.pl | awk -F/ '{print $NF}' | awk -F- '{print $1}'
 export EXECDIR SERVER NODE TYPES
 
 for SERVER in * ; do
+	test -L $SERVER && continue;
 	test -d $SERVER || continue;
 	cd $SERVER
 	for NODE in * ; do
+		test -L $NODE && continue;
+		test -d $NODE || continue;
 		cd $NODE
 		echo "$SERVER/$NODE:"
 		sh ../../refile-and-grok-node.sh &
