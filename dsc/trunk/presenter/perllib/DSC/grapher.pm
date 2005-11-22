f77b87dc05cdd1a3f0faff76b81e65a58ac8ec4c
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2005-11-22 20:42:47 +0000

    move valid TLD (domain) configuration to dsc-grapher.cfg

--- dsc/trunk/presenter/perllib/DSC/grapher.pm
+++ dsc/trunk/presenter/perllib/DSC/grapher.pm
@@ -42,21 +42,6 @@ my $dbg_lvl = 2;
 my $DATAROOT = '/usr/local/dsc/data';
 my $CacheImageTTL = 60; # 1 min
 my $expires_time = '+1m';
-my @valid_tlds = qw(
-ac ad ae af ag ai al am an ao aq ar as at au aw az ba bb bd be bf
-bg bh bi bj bm bn bo br bs bt bv bw by bz ca cc cd cf cg ch ci ck
-cl cm cn co cr cu cv cx cy cz de dj dk dm do dz ec ee eg eh er es
-et fi fj fk fm fo fr ga gd ge gf gg gh gi gl gm gn gp gq gr gs gt
-gu gw gy hk hm hn hr ht hu id ie il im in io iq ir is it je jm jo
-jp ke kg kh ki km kn kp kr kw ky kz la lb lc li lk lr ls lt lu lv
-ly ma mc md mg mh mk ml mm mn mo mp mq mr ms mt mu mv mw mx my mz
-na nc ne nf ng ni nl no np nr nu nz om pa pe pf pg ph pk pl pm pn
-pr ps pt pw py qa re ro ru rw sa sb sc sd se sg sh si sj sk sl sm
-sn so sr st sv sy sz tc td tf tg th tj tk tm tn to tp tr tt tv tw
-tz ua ug uk um us uy uz va vc ve vg vi vn vu wf ws ye yt yu za zm
-zw aero biz com coop info museum name net org pro gov edu mil int
-arpa .
-);
 
 my $use_data_uri;
 my %ARGS;	# from CGI
@@ -961,9 +946,13 @@ sub image_to_buf {
 	$buf;
 }
 
+my @valid_domains = ();
+
 sub valid_tld_filter {
 	my $tld = shift;
-	grep {$tld eq $_} @valid_tlds;
+	@valid_domains = DSC::grapher::config::get_valid_domains($ARGS{server})
+		unless @valid_domains;
+	grep {$_ && $tld eq $_} @valid_domains;
 }
 
 sub numeric_tld_filter {
--- dsc/trunk/presenter/perllib/DSC/grapher/config.pm
+++ dsc/trunk/presenter/perllib/DSC/grapher/config.pm
@@ -47,9 +47,33 @@ sub read_config {
 		if ($directive eq 'timezone') {
 			$ENV{TZ} = $x[0];
 		}
+		if ($directive eq 'domain_list') {
+			my $listname = shift @x;
+			die "Didn't find list-name after domain_list" unless defined($listname);
+			push(@{$CONFIG{domain_list}{$listname}}, @x);
+		}
+		if ($directive eq 'valid_domains') {
+			my $server = shift @x;
+			die "Didn't find server-name after valid_domains" unless defined($server);
+			my $listname = shift @x;
+			die "domain list-name $listname does not exist"
+				unless defined($CONFIG{domain_list}{$listname});
+			$CONFIG{valid_domains}{$server} = $listname;
+		}
 	}
 	close(F);
 	\%CONFIG;
+}
+
+sub get_valid_domains {
+	my $server = shift;
+	print STDERR "get_valid_domains: server is $server\n";
+	my $listname = $CONFIG{valid_domains}{$server};
+	print STDERR "get_valid_domains: listname is $listname\n";
+	return (undef) unless defined ($listname);
+	return (undef) unless defined ($CONFIG{domain_list}{$listname});
+	print STDERR "get_valid_domains: $server valid domain list is $listname\n";
+	@{$CONFIG{domain_list}{$listname}};
 }
 
 1;
