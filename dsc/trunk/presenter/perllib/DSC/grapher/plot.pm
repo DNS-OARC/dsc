8c16d4b3a2806661bcfd205d7163f15ba884bb2d
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2005-12-07 22:36:06 +0000

    Added SERVFAIL as an interesting rcode value

--- dsc/trunk/presenter/perllib/DSC/grapher/plot.pm
+++ dsc/trunk/presenter/perllib/DSC/grapher/plot.pm
@@ -140,9 +140,9 @@ my $std_accum_yaxes = {
   },
 
   rcode => {
-    keys	=> [ qw(0 3 5 8 else) ],
-    names	=> [ qw(NOERROR NXDOMAIN REFUSED NXRRSET Other) ],
-    colors	=> [ qw(brightgreen red blue orange purple) ],
+    keys	=> [ qw(0 2 3 5 8 else) ],
+    names	=> [ qw(NOERROR SERVFAIL NXDOMAIN REFUSED NXRRSET Other) ],
+    colors	=> [ qw(brightgreen yellow red blue orange purple) ],
     data_reader => \&DSC::extractor::read_data,
     data_summer => \&DSC::grapher::data_summer_1d,
     plot_type	=> 'trace',
