ef7b323accbd7916b80cdcc321648e78a988036b
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2006-01-26 17:35:51 +0000

    adding

---
+++ dsc/trunk/presenter/extractor/fix-corrupted-dat-files.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Digest::MD5;
+
+my $MD5_frequency = 500;
+
+foreach my $fn (@ARGV) {
+	do_file($fn);
+}
+
+
+sub do_file {
+    my $fn = shift;
+    open(I, $fn) or die "$fn: $!";
+    open(O, ">$fn.fixed") or die "$fn.fixed: $!";
+    my $nl = 0;
+    my $md = Digest::MD5->new;
+    my $buf = '';
+    my $errs = 0;
+    while (<I>) {
+        $nl++;
+	$buf .= $_;
+        if (/^#MD5 (\S+)/) {
+                if ($1 ne $md->hexdigest) {
+                        warn "$fn: MD5 checksum error at line $nl\n";
+                        $errs++;
+                } else {
+			#print "$fn: good up to line $nl\n";
+			print O $buf;
+		}
+		$buf = '';
+                next;
+        }
+        $md->add($_);
+    }
+    if ($errs == 0) {
+	unlink("$fn.fixed");
+#	print "$fn: okay\n";
+    } else {
+	rename ("$fn", "$fn.bad");
+	rename ("$fn.fixed", "$fn");
+	print "$fn: FIXED\n";
+    }
+}
