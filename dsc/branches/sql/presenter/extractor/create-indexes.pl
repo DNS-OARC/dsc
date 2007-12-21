f63af2757b65743047e6d8f672b87d728f664305
kkeys <kkeys@77dcf3b2-6f72-da11-8bdf-00902717936e>
2007-12-21 19:13:10 +0000

    more informative output

--- dsc/branches/sql/presenter/extractor/create-indexes.pl
+++ dsc/branches/sql/presenter/extractor/create-indexes.pl
@@ -15,13 +15,15 @@ read_config("$DSCDIR/etc/dsc-extractor.cfg");
 
 my $dbh = get_dbh(AutoCommit => 1);
 my $start = time;
+my $n = 0;
 for my $name (@{&data_table_names($dbh)}) {
     print "creating indexes for $name\n";
     create_data_indexes($dbh, $name);
     my $now = time;
     print "created indexes for $name in ", $now - $start, " seconds \n";
     $start = $now;
+    $n++;
 }
-print "done.\n";
+print "Done creating indexes for $n tables.\n";
 
 
