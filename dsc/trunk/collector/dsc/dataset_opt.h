39111860af9aa896bf7f8184456328a2e3bd90ba
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2007-12-27 23:53:08 +0000

    knew I'd forget to commit this...

---
+++ dsc/trunk/collector/dsc/dataset_opt.h
@@ -0,0 +1,9 @@
+#ifndef DATASET_OPT_H
+#define DATASET_OPT_H
+
+typedef struct {
+    int min_count;	// min cell count to report
+    int max_cells;	// max 2nd dim cells to print
+} dataset_opt;
+
+#endif
