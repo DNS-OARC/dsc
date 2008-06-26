d0017fa6bbff3927b0873909fd33f771c2486843
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2008-06-26 20:52:21 +0000

    Try using a couple of "index" arrays, rather than the port number as index, to reduce memory usage.

--- dsc/trunk/collector/dsc/dns_source_port_index.c
+++ dsc/trunk/collector/dsc/dns_source_port_index.c
@@ -2,24 +2,27 @@
 #include <assert.h>
 #include <stdio.h>
 #include <netdb.h>
+#include <memory.h>
 
 #include "dns_message.h"
 #include "md_array.h"
 
 /* Indexes the source port of DNS messages */
 
-static char inuse[65536];
-static int largest = 0;
+static unsigned int f_index[65536];
+static unsigned short r_index[65536];
+static unsigned int largest = 0;
 
 int
 dns_source_port_indexer(const void *vp)
 {
     const dns_message *m = vp;
-    int i = (int) m->tm->src_port;
-    if (i > largest)
-	largest = i;
-    inuse[i] = 1;
-    return i;
+    unsigned short p = m->tm->src_port;
+    if (0 == f_index[p]) {
+	f_index[p] = ++largest;
+	r_index[largest] = p;
+    }
+    return f_index[p];
 }
 
 static int next_iter = 0;
@@ -29,21 +32,19 @@ dns_source_port_iterator(char **label)
 {
     static char label_buf[20];
     if (NULL == label) {
-	for (next_iter = 0; !inuse[next_iter]; next_iter++);
+	next_iter = 0;
 	return largest + 1;
     }
     if (next_iter > largest)
 	return -1;
-    snprintf(*label = label_buf, 20, "%d", next_iter);
-    for (next_iter++; !inuse[next_iter]; next_iter++);
+    snprintf(*label = label_buf, 20, "%hu", r_index[next_iter++]);
     return next_iter;
 }
 
 void
 dns_source_port_reset(void)
 {
-    for(; largest; largest--)
-	inuse[largest] = 0;
-    /* NOTE, now largest == 0 */
+    memset(f_index, 0, sizeof f_index);
+    memset(r_index, 0, sizeof r_index);
+    largest = 0;
 }
-
