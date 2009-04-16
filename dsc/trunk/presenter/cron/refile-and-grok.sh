73df07c467f5e31182586a676d5fe35b3398dc0e
wessels <wessels@77dcf3b2-6f72-da11-8bdf-00902717936e>
2009-04-16 04:35:50 +0000

    # Uncomment the below test to skip nodes that don't # have an incoming directory.  It is useful if you # receive pre-grokked data to this presenter, but # NOT useful if you need backward compatibility with # older collectors that do not put XMLs into an # incoming subdirectory

--- dsc/trunk/presenter/cron/refile-and-grok.sh
+++ dsc/trunk/presenter/cron/refile-and-grok.sh
@@ -19,7 +19,15 @@ for SERVER in * ; do
 	for NODE in * ; do
 		test -L $NODE && continue;
 		test -d $NODE || continue;
-		test -d $NODE/incoming || continue;
+		#
+		# Uncomment the below test to skip nodes that don't
+		# have an incoming directory.  It is useful if you
+		# receive pre-grokked data to this presenter, but
+		# NOT useful if you need backward compatibility with
+		# older collectors that do not put XMLs into an
+		# incoming subdirectory
+		#
+		#test -d $NODE/incoming || continue;
 		cd $NODE
 		echo "$SERVER/$NODE:"
 		$EXECDIR/dsc-xml-extractor.pl >dsc-xml-extractor.out 2>&1 # &
