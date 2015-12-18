2e2f90fbb92f50814e92ca4fe812f85aed483e02
McStork <macstork@gmail.com>
2015-12-18 12:28:44 -0800

    Give option for additional output JSON/Extended JSON
    * Add a new option 'add_output' in the config file. Possible values are json
     and ext_json.
    * add_output can only be set once in the config file.
    * json outputs date as iso8601.
    * ext_json dates comply to the format specified by Extended JSON.

--- collector/dsc/dns_message.h
+++ collector/dsc/dns_message.h
@@ -10,6 +10,7 @@
 
 #include "inX_addr.h"
 #include "dataset_opt.h"
+#include "md_array.h"
 
 #define MAX_QNAME_SZ 512
 
@@ -52,11 +53,10 @@ struct _dns_message
     /* ... */
 };
 
-void dns_message_report(FILE *);
-int dns_message_add_array(const char *, const char *, const char *, const char *, const char *, const char *,
-    dataset_opt);
-const char *dns_message_QnameToNld(const char *, int);
-const char *dns_message_tld(dns_message * m);
+void dns_message_report(FILE *, md_array_printer *);
+int dns_message_add_array(const char *, const char *,const char *,const char *,const char *,const char *, dataset_opt);
+const char * dns_message_QnameToNld(const char *, int);
+const char * dns_message_tld(dns_message * m);
 void dns_message_init(void);
 void dns_message_clear_arrays(void);
 void dns_message_handle(dns_message *);
