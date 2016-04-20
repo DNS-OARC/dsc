05ef69980556e69e3246b13d20129e5f1176a70c
Jerry Lundström <lundstrom.jerry@gmail.com>
2016-04-20 13:06:33 +0200

    Fix make test in dist.

--- src/test/test1.sh
+++ src/test/test1.sh
@@ -1,15 +1,15 @@
-#!/bin/sh -e
+#!/bin/sh -xe
 
-../dsc ./1458044657.conf
+../dsc "$srcdir/1458044657.conf"
 
 test -f 1458044657.dscdata.json || sleep 1
 test -f 1458044657.dscdata.json || sleep 2
 test -f 1458044657.dscdata.json || sleep 3
 test -f 1458044657.dscdata.json
-diff 1458044657.dscdata.json 1458044657.json_gold
+diff 1458044657.dscdata.json "$srcdir/1458044657.json_gold"
 
 test -f 1458044657.dscdata.xml || sleep 1
 test -f 1458044657.dscdata.xml || sleep 2
 test -f 1458044657.dscdata.xml || sleep 3
 test -f 1458044657.dscdata.xml
-diff 1458044657.dscdata.xml 1458044657.xml_gold
+diff 1458044657.dscdata.xml "$srcdir/1458044657.xml_gold"
