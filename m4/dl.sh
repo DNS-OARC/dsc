5789f8bbeeec029c32041184659bdd5a2d1b7a33
Jerry Lundström <lundstrom.jerry@gmail.com>
2020-03-20 10:18:57 +0100

    Release 2.9.0

--- m4/dl.sh
+++ m4/dl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-# Copyright (c) 2019, OARC, Inc.
+# Copyright (c) 2020, OARC, Inc.
 # All rights reserved.
 #
 # This file is part of dnsjit.
--- src/hashtbl.h
+++ src/hashtbl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2019, OARC, Inc.
+ * Copyright (c) 2008-2020, OARC, Inc.
  * Copyright (c) 2007-2008, Internet Systems Consortium, Inc.
  * Copyright (c) 2003-2007, The Measurement Factory, Inc.
  * All rights reserved.
